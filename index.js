d0259c8c32f2a044b9da99413a0cdaaccc3be007
ftvalex01 <ftvalex@hotmail.com>
2024-01-28 15:51:38 +0000

    terminando

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const multer = require('multer');
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-
+// Set up multer for file uploads
+const upload = multer({ dest: 'uploads/' });
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
+
+  const fileMetadata = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  };
+
+  res.json(fileMetadata);
+});
 
 
 
